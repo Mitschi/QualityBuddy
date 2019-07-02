7800ad7777ee79122c26e5a12cdd795ac3924e57
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-02 12:31:22 +0200

    Backend folder/file structure

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
+import { BuildModule } from './build/build.module';
 import { RepoModule } from './repo/repo.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/qualitybuddy'), RepoModule],
+  imports: [BuildModule, RepoModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- backend/src/model/repo.schema.ts
+++
@@ -1,5 +0,0 @@
-import * as  mongoose from 'mongoose';
-
-export const RepoSchema = new mongoose.Schema({
-
-});
