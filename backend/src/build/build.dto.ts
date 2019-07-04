6c6883edc1f2de30406ba6f19e40fb4f296e934d
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-04 10:22:05 +0200

    Added dto validation

--- backend/src/build/build.dto.ts
+++ backend/src/build/build.dto.ts
@@ -1,10 +1,19 @@
-export interface BuildDTO {
+import { IsNotEmpty, IsString, IsNumber, IsDate } from 'class-validator';
+
+export class BuildDTO {
+    @IsString()
     state: string;
+    @IsNumber()
     duration: number;
+    @IsDate()
     started_at: Date;
+    @IsDate()
     finished_at: Date;
+    @IsNumber()
     number: number;
+    @IsString()
     repo_id: string;
+    @IsNotEmpty()
     commit: {
         id: string;
         sha: string;
--- backend/src/repo/repo.dto.ts
+++ backend/src/repo/repo.dto.ts
@@ -1,7 +1,15 @@
-export interface RepoDto {
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class RepoDto {
+    @IsString()
     name: string;
+    @IsString()
+    @IsNotEmpty()
     id: string;
+    @IsString()
     slug: string;
+    @IsString()
     href: string;
+    @IsString()
     token: string;
 }
