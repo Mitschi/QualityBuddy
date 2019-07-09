824570bdc96237a93e605d2a8633a582b42df94d
TimoZuerner <timo.zuerner@htl-klu.at>
2019-07-09 15:03:50 +0200

    Created Chart

--- test-fronted/show-build.js
+++ test-fronted/show-build.js
@@ -1,116 +1,182 @@
-import  {fetchBuild} from ('./fetch-build')
-
-/**
-[
-  {
-    "commit": {
-      "id": "167279302",
-      "message": "Added eslint to dependencies",
-      "committed_at": "2019-07-02T08:28:04.000Z",
-      "sha": "b18f78c26bd8009579cf91b891aa39c334d49134"
-    },
-    "_id": "5d2440e7118bcf03bc488fa3",
-    "number": 2,
-    "state": "passed",
-    "started_at": "2019-07-02T08:29:03.000Z",
-    "finished_at": "2019-07-02T08:29:58.000Z",
-    "duration": 140,
-    "repo_id": "25171564",
-    "__v": 0
-  },
-  {
-    "commit": {
-      "id": "167278820",
-      "message": "Added travis ci and husky precommit testing",
-      "committed_at": "2019-07-02T08:24:10.000Z",
-      "sha": "7c7a1dab8193b8b7509427259d5e9ff1911d1296"
-    },
-    "_id": "5d2440e7118bcf03bc488fa4",
-    "number": 1,
-    "state": "failed",
-    "started_at": "2019-07-02T08:24:59.000Z",
-    "finished_at": "2019-07-02T08:25:51.000Z",
-    "duration": 144,
-    "repo_id": "25171564",
-    "__v": 0
-  }
-]
-* */
 
 
+let build
+buildNumber = []
+buildTime = []
+chartColors = []
+stateDays = []
 
-fetchBuild.fetchBuild()
-console.log(fetchBuild.build)
 
-let buildTime = [44, 55, 57, 56, 61, 58, 63, 60, 66]
-let buildNumber = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
-
-function showStats(){
-
-    var options = {
-    chart: {
-        height: 350,
-        type: 'bar',
-    },
-    plotOptions: {
-        bar: {
-            horizontal: false,
-            columnWidth: '55%',
-            endingShape: 'flat'	
-        },
-    },
-    dataLabels: {
-        enabled: false
-    },
-    series: [{
-        name: 'Build Time',
-        data: buildTime
-    }],
-    xaxis: {
-        categories: buildNumber,
-        title: {
-            text: 'Build Number',
-            style: {
-                color: undefined,
-                fontSize: '19px',
-                fontFamily: 'Helvetica, Arial, sans-serif',
-                cssClass: 'apexcharts-yaxis-title',
-            },
-        }
-    },
-    yaxis: {
-        title: {
-            text: 'Build Time [s]',
-            style: {
-                color: undefined,
-                fontSize: '19px',
-                fontFamily: 'Helvetica, Arial, sans-serif',
-                cssClass: 'apexcharts-yaxis-title',
-            },
-        }
-    },
-    fill: {
-        opacity: 1
-
-    },
-
-    tooltip: {
-        y: {
-            formatter: function (val) {
-                return val + "Seconds"
-            }
-        }
-    }
-    }
-
-    var chart = new ApexCharts(
-        document.querySelector("#chart"),
-        options
-    );
-
-    chart.render();
+function fetchBuild(){
+    axios.get("http://localhost:3000/build")
+    .then((response) => {
+      build = response.data
+      collectStats()
+    })
 }
 
 
+
+
+function collectStats(){
+    let lastDay
+    let counter = 0
+    build.forEach(element => {
+        buildNumber.splice(0, 0, element['number'])
+        buildTime.splice(0, 0, element['duration'])
+        
+        const date = new Date(element.started_at)
+        const year = date.getFullYear()
+        const month = date.getMonth()
+        const day = date.getDay()
+        
+        stateDays.push(day)
+        if(element['state'] == "passed"){
+            chartColors.splice(0, 0, "#00ff00")
+        }else{
+            chartColors.splice(0, 0, "#ff0000")
+        } 
+        
+    });
+    console.log(stateDays)
+}
+
+
+
+function showStats(){
+ 
+
+
+    var build_stats_options = {
+        chart: {
+            height: 350,
+            type: 'bar',
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: '55%',
+                endingShape: 'flat',
+            },
+        },
+        dataLabels: {
+            enabled: false,
+        },
+        series: [{
+            name: 'Build Time',
+            data: buildTime,
+        }],
+        xaxis: {
+            categories: buildNumber,
+            title: {
+                text: 'Build Number',
+                style: {
+                    color: undefined,
+                    fontSize: '19px',
+                    fontFamily: 'Helvetica, Arial, sans-serif',
+                    cssClass: 'apexcharts-yaxis-title',
+                },
+            }
+        },
+        yaxis: {
+            title: {
+                text: 'Build Time [s]',
+                style: {
+                    color: undefined,
+                    fontSize: '19px',
+                    fontFamily: 'Helvetica, Arial, sans-serif',
+                    cssClass: 'apexcharts-yaxis-title',
+                },
+            }
+        },
+        fill: {
+            opacity: 1,
+        },
+        tooltip: {
+            style: {
+                fontSize: '12px',
+                fontFamily: undefined,
+              },
+              marker: {
+                show: false,
+            },
+            y: {
+                formatter: function (val) {
+                    return val + "Seconds"
+                }
+                
+            }
+        },
+        fill: {
+            colors: [function() {
+            if(chartColors[0] == "#00ff00") {
+                chartColors.splice(0, 1)
+                return '#00ff00'
+            } else{
+                chartColors.splice(0, 1)
+                return '#ff0000'
+                }
+                }]
+        }
+    }
+  
+   
+    var build_stats = new ApexCharts(
+        document.querySelector("#chart"),
+        build_stats_options
+    );
+    build_stats.render()
+   
+    var build_succes_chart_options = {
+            chart: {
+              height: 350,
+              type: 'line',
+              zoom: {
+                enabled: false
+              }
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+              curve: 'straight'
+            },
+            series: [{
+              name: "Succes",
+              data:  [1, 3, 55]
+            },
+            {
+                name: "Fails",
+                data: [2, 123, 123 ]
+              }],
+            title: {
+              text: 'Product Trends by Month',
+              align: 'left'
+            },
+            grid: {
+              row: {
+                colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
+                opacity: 0.5
+              },
+            },
+            xaxis: {
+            type: 'datetime',
+              categories: stateDays
+            }
+          }
+      var build_succes_chart = new ApexCharts(
+        document.querySelector("#chart1"),
+        build_succes_chart_options
+      );
+  
+      build_succes_chart.render();
+  
+      
+  
+
+    
+}
+
+fetchBuild()
 
 showStats()
