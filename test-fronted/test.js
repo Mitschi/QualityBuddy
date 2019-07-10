a0c809472d6a0fa727bd72af54d39d1b59345259
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-10 08:59:24 +0200

    Fixed date sorting bug

--- test-fronted/test.js
+++ test-fronted/test.js
@@ -1,25 +1,27 @@
 const axios = require('axios')
+const dateformat = require('dateformat')
 
 let response 
 const date = []
 
 async function getData() {
     response = await axios({
-        url: 'http://localhost:4000/build',
+        url: 'http://localhost:4000/build/25171564',
         method: 'get'
       })
     
       response.data.forEach(element => {
         const buildDate = new Date(element.started_at)
-        date.push({date: buildDate.toISOString(), state: element.state})
+        const formatedBuildDate = dateformat(buildDate, "yyyy-mm-dd")
+        date.push({date: formatedBuildDate, state: element.state})
     });
     
     const result = Object.values(date.reduce((r, o) => {
-        r[o.Date] = r[o.Date] || {Date: o.date, passed : 0, failed: 0};
+        r[o.date] = r[o.date] || {date: o.date, passed : 0, failed: 0};
         if(o.state === 'passed'){
-            r[o.Date].passed ++;
+            r[o.date].passed ++;
         } else {
-            r[o.Date].failed ++;
+            r[o.date].failed ++;
         }
         
         return r;
