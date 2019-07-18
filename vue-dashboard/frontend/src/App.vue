4ae41db9e12b1673155c342dce0e67c56ab443eb
prakt09 <prakt09@ifit-e.uni-klu.ac.at>
2019-07-18 11:53:26 +0200

    Created Dashboard Files

--- vue-dashboard/frontend/src/App.vue
+++ vue-dashboard/frontend/src/App.vue
@@ -3,6 +3,23 @@
     <hello-world></hello-world>
   </div>
 </template>
+<script>
+
+import Vue from 'vue'
+import Vuesax from 'vuesax'
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+  Vue.use(Vuesax)
+
+
+
+export default {
+  name: 'App',
+  components: {
+    SideBar
+  }
+}
+</script>
 
 <style lang="scss">
 #app {
--- vue-dashboard/frontend/src/main.js
+++ vue-dashboard/frontend/src/main.js
@@ -3,7 +3,10 @@ import VueChartkick from 'vue-chartkick';
 import Chart from 'chart.js';
 import App from './App.vue';
 import HelloWorld from './components/HelloWorld.vue';
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css' //Vuesax styles
 
+Vue.use(Vuesax)
 Vue.config.productionTip = false;
 Vue.component('hello-world', HelloWorld);
 Vue.use(VueChartkick, { adapter: Chart });
