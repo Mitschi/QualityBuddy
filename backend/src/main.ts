8c6d414b26514f1cfb3e5d883b65f92ec13e9f9d
Tanner Gabriel <gabrieltanner.code@gmail.com>
2019-07-18 14:38:00 +0200

    Added swagger documentation to the backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,5 +1,6 @@
 import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import * as helmet from 'helmet';
@@ -9,6 +10,16 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe());
   app.use(helmet());
   app.enableCors();
+
+  const options = new DocumentBuilder()
+  .setTitle('QualityBuddy Backend')
+  .setDescription('Backend for the QualityBuddy application which saves the builds and sonarqube project infos')
+  .setVersion('1.0')
+  .addTag('nest')
+  .build();
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
