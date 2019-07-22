80056d276201cbaf3483bfea8ab29fc2d844db95
John Doe <johndoe@example.com>
2019-07-22 09:34:45 +0200

    Added App.vue

---
+++ vue-dashboard/frontend/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <SideBar/>
+  </div>
+</template>
+
+<script>
+import SideBar from './components/SideBar'
+
+export default {
+  name: 'App',
+  components: {
+    SideBar
+  }
+}
+</script>
+
+<style>
+
+</style>
