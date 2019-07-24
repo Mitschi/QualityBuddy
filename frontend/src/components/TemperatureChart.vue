ebb8cedd92e7c5a4b50ab657924505179ff030ff
TimoZuerner <timo.zuerner@htl-klu.at>
2019-07-24 11:17:58 +0200

    Added possibility to fetch Temperatur and Humidity from Database

---
+++ frontend/src/components/TemperatureChart.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <apexchart v-if="loaded" type="line" :options="temperaturChartOptions" :series="temperatureSeries"></apexchart>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import dateformat from "dateformat";
+
+@Component({
+  components: {},
+  props: {
+    result: {
+      required: true
+    }
+  }
+})
+export default class BuildStateChart extends Vue {
+  @Prop({ default: "result" })
+  result;
+ 
+     temperaturChartOptions = {
+    chart: {
+      id: "temperature-chart",
+      foreColor: "#fff"
+    },
+    xaxis: {
+      categories: []
+    },
+    title: {
+      text: "Build States",
+      align: "left",
+      style: {
+        fontSize: "12px"
+      }
+    },
+    fill: {
+      colors: []
+    }
+  };
+
+  temperatureSeries = [
+    {
+      name: "Temperature",
+      data: []
+    },
+    {
+        name: "Humidity",
+        data: []
+    },
+    {
+      name: "Build Fails",
+      data: []
+    }
+  ];
+
+
+}
+ 
+</script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,12 +1,11 @@
-import Vue from 'vue';
-import App from './App.vue';
-import VueApexCharts from 'vue-apexcharts'
-Vue.use(VueApexCharts)
- 
-Vue.component('apexchart', VueApexCharts)
+import Vue from "vue";
+import App from "./App.vue";
+import VueApexCharts from "vue-apexcharts";
 
+Vue.use(VueApexCharts);
+Vue.component("apexchart", VueApexCharts);
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app');
+  render: h => h(App)
+}).$mount("#app");
