edc40a5ffa7ad1a05064223b79e5165e55972a1d
jublebi <julianlechner31@gmail.com>
2019-07-29 08:50:57 +0200

    test components

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
-import VueApexCharts from 'vue-apexcharts'
+import VueApexCharts from 'vue-apexcharts';
+
 Vue.use(VueApexCharts)
  
 Vue.component('apexchart', VueApexCharts)
